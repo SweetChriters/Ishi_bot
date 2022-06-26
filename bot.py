34280edc54256596f807ba48e85fa949949e9649
SweetIshi <96017677+SweetChriters@users.noreply.github.com>
2022-06-26 23:21:02 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,29 @@
 import discord
 from discord.ext import commands
+import json
+with open('settings.json', 'r', encoding='utf8') as jfile:
+    jdata = json.load(jfile)
 
-bot = commands.Bot(command_prefix= '!')
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix= '!', intents = intents)
 
 @bot.event
 async def on_ready():
     print(">> Bot is online <<")
 
-bot.run('OTkwMjg0NjA2NDQxOTM4OTQ0.GXPASW.qsfnkdUjeksoYj8fMsJKV8pQbcH9549IHbBAN4')
+@bot.event
+async def on_member_join(member):
+    channel = bot.get_channel(int(jdata['Welcome_channel']))
+    await channel.send(f'{member} Join!')
+
+@bot.event
+async def on_member_remove(member):
+    channel = bot.get_channel(int(jdata['Leave_channel']))
+    await channel.send(f'{member} Leave!')
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send(f'{round(bot.latency*1000)} (ms)')
+
+
+bot.run(jdata['TOKEN'])
